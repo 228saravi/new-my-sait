18b4c125fc37e05b895d3e2efb90febd6f24fc34
228saravi <228saravi@gmail.com>
2018-07-10 15:28:44 +0300

    indeximg

--- gulp/task/sass.js
+++ gulp/task/sass.js
@@ -1,7 +1,7 @@
 `use strict`
 module.exports = ()=>{
     $.gulp.task('sass', function() {
-        return $.gulp.src('./source/style/*/*.scss')
+        return $.gulp.src('./source/style/main.scss')
         .pipe($.gp.sourcemaps.init())
         .pipe($.gp.sass())
         .on('error',$.gp.notify.onError({
