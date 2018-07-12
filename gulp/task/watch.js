077eb174520fa7ca7a0983a4d0980a6c26eceec0
228saravi <228saravi@gmail.com>
2018-07-12 16:48:50 +0300

    watch

--- gulp/task/watch.js
+++ gulp/task/watch.js
@@ -6,6 +6,7 @@
             $.gulp.watch(element[0],$.gulp.series(element[1]));
         }); */
          $.gulp.watch('./source/style/*.scss',$.gulp.series('sass'));
+         $.gulp.watch('./source/style/*/*.scss',$.gulp.series('sass'));
          $.gulp.watch('./source/template/*.pug',$.gulp.series('pug'));
          $.gulp.watch('./source/img/*',$.gulp.series('img'));
      })
