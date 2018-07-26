6fbe94dbaa8207fee21e32fcc0235cfc88b121ac
228saravi <228saravi@gmail.com>
2018-07-26 16:59:09 +0300

    сщь

--- build/js/main.js
+++ build/js/main.js
@@ -1,19 +1,4 @@
-var blur=(function(){
-    
-    return{
-        set:function(){
-            var bgblur=document.querySelector('.blur');
-            var bgWidth=document.querySelector('.auth').offsetWidth;
-             
-            bgblur.style.backgroundSize = bgWidth + 'px';
+"use strict";
 
-        }
-    }
-}())
-
-blur.set(); 
-
-
-window.onresize=()=>{
-    blur.set();
-}
+(function () {});
+//# sourceMappingURL=main.js.map
