bcdcc54e9661be36b6855929af4f64f46009d46a
228saravi <228saravi@gmail.com>
2018-07-27 13:35:37 +0300

    commit preloader

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,62 @@
-"use strict";
+'use strict';
 
-(function () {});
+var preloader = function () {
+  var percentsTotal = 0,
+      preloader = $('.preloader');
+
+  var imgPath = $('*').map(function (ndx, element) {
+    var background = $(element).css('background-image'),
+        img = $(element).is('img'),
+        path = '';
+
+    if (background != 'none') {
+      path = background.replace('url("', '').replace('")', '');
+    }
+
+    if (img) {
+      path = $(element).attr('src');
+    }
+
+    if (path) return path;
+  });
+
+  var setPercents = function setPercents(total, current) {
+    var persents = Math.ceil(current / total * 100);
+
+    $('.preloader__percents').text(persents + '%');
+
+    if (persents >= 100) {
+      preloader.fadeOut();
+    }
+  };
+
+  var loadImages = function loadImages(images) {
+
+    if (!images.length) preloader.fadeOut();
+
+    images.forEach(function (img, i, images) {
+      var fakeImage = $('<img>', {
+        attr: {
+          src: img
+        }
+      });
+
+      fakeImage.on('load error', function () {
+        percentsTotal++;
+        setPercents(images.length, percentsTotal);
+      });
+    });
+  };
+
+  return {
+    init: function init() {
+      var imgs = imgPath.toArray();
+
+      loadImages(imgs);
+    }
+  };
+}();
+$(function () {
+  preloader.init();
+});
 //# sourceMappingURL=main.js.map
